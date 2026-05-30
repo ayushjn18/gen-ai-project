dbc29eebceda9e03acff3517752dd43df8326024
Virat Kohli <ayushdiazoniumsalt@gmail.com>
2026-05-30 15:18:35 +0530

    Initial commit

---
+++ Frontend/src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import "./style.scss"
+import { AuthProvider } from "./features/auth/auth.context";
+
+createRoot(document.getElementById('root')).render(
+  <AuthProvider>
+    <App />
+  </AuthProvider>,
+)
